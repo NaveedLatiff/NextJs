458480f92e158f15a021fe8db4dccbad8aa69a59
Naveed latif <naveedlatif526@gmail.com>
2025-08-19 15:47:16 -0700

    Learn Image component

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='h-[200vh] bg-black text-white '>
       <h1 className='pt-[120px]'>I am Contact Page</h1>
+        <Image
+        src="/img.jpeg"  
+        alt="My photo"
+        width={700}
+        height={200}
+      />
     </div>
   )
 }
