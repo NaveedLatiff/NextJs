96b902233dc1c5f0a4ace4dac1c5aaf6884b8762
Naveed latif <naveedlatif526@gmail.com>
2025-08-20 03:27:28 -0700

    Learn nested routing

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Script from 'next/script'
 import React from 'react'
 
@@ -5,6 +6,10 @@ const page = () => {
     return (
         <div className='h-[200vh] bg-black text-white '>
             <h1 className='mx-auto pt-[120px]'>I am About Page</h1>
+            <p className='text-center mt-[20px]'>My Services</p>
+            <p><Link href='about/webdev'>Web Development</Link></p>
+            <p ><Link href='about/seo'>SEO</Link></p>
+
             <p className='mx-auto mt-[120vh]'>Testing Scroll</p>
             <Script id="my-script" strategy="beforeInteractive">
                 {`console.log("Hello World!")`}
---
+++ src/app/about/webdev/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-black text-center min-h-screen '>
+        <h1 className='text-white pt-[50px]'>I am Web Developer</h1>
+    </div>
+  )
+}
+
+export default page
